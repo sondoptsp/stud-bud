d331ae07739c2484ebe6e0acca0a59b592f92337
Do An Duc <anduk@Dos-MacBook-Pro.local>
2022-04-20 00:31:07 +0700

    fix gitignore one last time

--- server/dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.__prod__ = exports.COOKIE_NAME = void 0;
-exports.COOKIE_NAME = "studbud_session_cookie";
-exports.__prod__ = process.env.NODE_ENV === "production";
-//# sourceMappingURL=constants.js.map
--- server/dist/graphql/mutations/InterestMutation.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=InterestMutation.js.map
